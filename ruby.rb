d1f3a2d7e37f004a49e9e127539d2d6f4a5eacce
vikitaotiz <victorotieno598@gmail.com>
2021-04-21 19:51:47 +0300

    Fix linters

--- ruby.rb
+++ ruby.rb
@@ -2,11 +2,7 @@ def burble_sort(arr)
   len = 0
   while len < arr.length
     (arr.length - 1).times do |i|
-      if arr[i] > arr[i + 1]
-        temp = arr[i]
-        arr[i] = arr[i + 1]
-        arr[i + 1] = temp
-      end
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
     end
     len += 1
   end
@@ -15,22 +11,18 @@ end
 
 p = [9, 8, 7, 6, 5, 4, 3, 2, 1, 3, 4, 5, 6, 7, 8]
 
-burble_sort(p)
+p burble_sort(p)
 
 def burble_sort_by(arr)
   len = 0
   while len < arr.length
     (arr.length - 1).times do |i|
       sort_by = yield(arr[i], arr[i + 1])
-      if sort_by.positive?
-        temp = arr[i]
-        arr[i] = arr[i + 1]
-        arr[i + 1] = temp
-      end
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if sort_by.positive?
     end
     len += 1
   end
   arr
 end
 
-p burble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
+p burble_sort_by(%w[hi hello hey h]) { |left, right| left.length - right.length }
