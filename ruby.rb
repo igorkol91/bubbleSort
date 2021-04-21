7d36090cc118a9f21cf0b0950f10fbd43db16c29
vikitaotiz <victorotieno598@gmail.com>
2021-04-21 17:47:53 +0300

    Add burble_sort_by method

--- ruby.rb
+++ ruby.rb
@@ -1,4 +1,4 @@
-def sort(arr)    
+def burble_sort(arr)    
     for j in arr do
       (arr.length-1).times do |i|
         if arr[i] > arr[i+1]
@@ -13,4 +13,24 @@ def sort(arr)
   end
 
 p = [9,8,7,6,5,4,3,2,1,3,4,5,6,7,8]
+
 sort(p)
+
+def burble_sort_by(arr)
+    for j in arr do
+      (arr.length-1).times do |i|
+
+        sort_by = yield(arr[i], arr[i+1])
+
+        if sort_by.positive?
+            temp = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = temp
+        end
+
+    end
+  end
+  return arr
+end
+
+p sort_by(["hi","hello","hey"]) { |left, right| left.length - right.length }
