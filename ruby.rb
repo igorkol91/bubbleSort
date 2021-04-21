6b872d78d3eb438ec6c65d9656357007ea81f52d
vikitaotiz <victorotieno598@gmail.com>
2021-04-21 20:05:33 +0300

    Fix linters

--- ruby.rb
+++ ruby.rb
@@ -9,9 +9,7 @@ def burble_sort(arr)
   arr
 end
 
-p = [9, 8, 7, 6, 5, 4, 3, 2, 1, 3, 4, 5, 6, 7, 8]
-
-p burble_sort(p)
+p burble_sort([9, 8, 7, 6, 5, 4, 3, 2, 1, 3, 4, 5, 6, 7, 8])
 
 def burble_sort_by(arr)
   len = 0
