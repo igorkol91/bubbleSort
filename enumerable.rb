d74b356ca6d10d79e9e0e9a16df186a00de152d5
Igor.Koloski <koloski_igor@hotmail.com>
2021-04-23 11:55:27 +0200

    Fixing linters

--- enumerable.rb
+++ enumerable.rb
@@ -2,6 +2,7 @@ module Enumerable
   ## my_each method
   def my_each
     return self unless block_given?
+    
     arr = self
     for i in arr
         yield(i)
@@ -10,10 +11,11 @@ module Enumerable
 
   ## my_each_index method
   def my_each_with_index
-    return self unless block_given? 
+    return self unless block_given?
+
     arr = self
     i = 0
-    until i >= arr.length do
+    until i >= arr.length
         yield(arr[i], i)
         i += 1
     end
@@ -22,67 +24,75 @@ module Enumerable
   ## my_select method
   def my_select
     return self unless block_given?
+
     arr = []
     my_each { |i| arr << i if yield(i) }
-    return arr
+    arr
   end
 
   ## my_any method
   def my_any
     return self unless block_given?
+
     arr = self
     arr.my_each { |i| return true if yield(i) }
-    return false
+    false
   end
 
   ## my_all method
   def my_all
-    return unless  block_given?
+    return unless block_given?
+
     arr = self
     arr.my_each { |i| return false if !yield(i) }
-    return true
+    true
   end
 
   ## my_none method
   def my_none
-    return unless  block_given?
+    return unless block_given?
+
     arr = self
     arr.my_each { |i| return false if !yield(i) }
-    return true
+    true
   end
 
   ## my_count method
   def my_count
     return unless  block_given?
+
     arr = self
     counter = 0
     arr.my_each { |i| counter += 1 if yield(i) }
-    return counter   
+    counter
   end
 
   ## my_map method
   def my_map(&factor)
     return self unless block_given?
+
     arr = self
     new_arr = []
     arr.my_each { |x| new_arr << factor.call(x) }
-    return new_arr
+    new_arr
   end
 
   ## my_inject method
   def my_inject
     return self unless block_given?
+
     arr = self
     arr.my_each { |i| yield(i) }
   end
 
   ## multiply_els method
   def multiply_els
-    return self unless  block_given?
+    return self unless block_given?
+
     arr = self
     result = 1
     arr.my_inject { |i| result = yield(result, i) }
-    return result
+    result
   end
 end
 
@@ -91,12 +101,12 @@ puts '---- my_each ----'
 puts([2, 5, 6, 7].my_each { |x| x })
 
 ## my_each_index
-puts '---- my_each_index ----' 
-puts([2, 5, 6, 7].my_each_with_index { |x, i| puts i.to_s + ' : ' + x.to_s})
+puts '---- my_each_index ----'
+puts([2, 5, 6, 7].my_each_with_index { |x, i| puts "#{i.to_s} : #{x.to_s}" })
 
 ## my_select
 puts '---- my_select ----'
-puts([2, 5, 6, 7].my_select { |n| n if n })
+puts([2, 5, 6, 7].my_select { |n| n })
 
 ## my_any
 puts '---- my_any ----'
